69c391bca4c4b276b21d2168412095bb2d458390
wangzhe409 <wangzhe18@jd.com>
2022-05-27 23:29:28 +0800

    add antd echarts

--- src/pages/Home/index.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from './Layout';
-export default function IndexPage() {
-  return (
-    <div>
-      <Layout></Layout>
-      <iframe src="/react/index.html"></iframe>
-    </div>
-  );
-}
--- src/pages/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare type SVGComponent = React.FC<React.SVGProps<SVGSVGElement>>;
-
-declare type Option<T> = T | undefined;
-
-declare type Ok<T> = T;
-declare type Err<E> = E;
-declare type Result<T, E> = Ok<T> | Err<E>;
